67f1ec3cafc3588c2a14c8c868b4703d67504c78
Anisa KK <anisa.kouka@up2metric.com>
2023-07-05 22:28:28 +0300

    Initial commit for Spring Boot project

---
+++ src/main/java/com/cameinw/cameinwbackend/CameinwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cameinw.cameinwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameinwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameinwBackendApplication.class, args);
+	}
+
+}
